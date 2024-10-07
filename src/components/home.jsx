4fd68a1af6e692c8da2265ce2affe6b158a2af02
rsingad <rsingerjat@gmail.com>
2024-10-08 01:34:43 +0530

    voice supoter

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,29 @@
 import { NavLink } from "react-router-dom";
 import SocialMediaIcon from "./socialmediaicon";
+import { useEffect, useState } from "react";
 
 function Home() {
+  
+  const [text, setText] = useState("hellow");
+
+  // Function to speak the text
+  const speak = (text) => {
+    let text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.rate = 1; // Speech rate (1 is normal speed)
+    text_speak.pitch = -50; // Pitch of the voice
+    text_speak.volume = 100; // Volume level
+    window.speechSynthesis.speak(text_speak);
+  };
+  useEffect(()=>{
+    setTimeout(()=>{
+      speak("Hellow   i am  Ramesh singad and i' a frontend developer");
+    },1000);
+  },[])
+
+
   return (
     <section className="home">
+     
       <div className="leftaside">
         <div className="leftcontent">
           <h2>Hellow ,it's me</h2>
@@ -13,18 +33,18 @@ function Home() {
         <div>
           <p className="pra">more text own write...</p>
         </div>
-        <SocialMediaIcon/>
+        <SocialMediaIcon />
         <div className="moreButton">
           <button>more about me</button>
         </div>
+                  
       </div>
       <div className="rightaside">
-      <div className="lightCircal">
-        <div className="imgbox">
-          <img src="./public/my.jpg" alt="image" />
+        <div className="lightCircal">
+          <div className="imgbox" onClick={()=>speak("hellow boss....")}>
+            <img src="./public/my.jpg" alt="image" />
+          </div>
         </div>
-
-      </div>
       </div>
     </section>
   );
